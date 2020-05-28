e2cc5d14df838c476ed9c5091e02d5ba9a1402d0
fly_with24 <fly_with24@163.com>
2020-05-28 09:34:11 +0800

    add BuildConfig

---
+++ version_config/src/main/java/com/flywith24/version_config/BuildConfig.kt
@@ -0,0 +1,16 @@
+package com.flywith24.version_config
+
+/**
+ * @author Flywith24
+ * @date   2020/5/27
+ * time   10:59
+ * description
+ */
+object BuildConfig {
+    const val compileSdkVersion = 29
+    const val buildToolsVersion = "29.0.3"
+    const val minSdkVersion = 23
+    const val targetSdkVersion = 29
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
