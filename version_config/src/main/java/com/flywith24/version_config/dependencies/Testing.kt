b41a4db324d8489fd5a971a716b9be6beffd15e9
fly_with24 <fly_with24@163.com>
2020-05-28 09:42:09 +0800

    add androidRunner dependency

--- version_config/src/main/java/com/flywith24/version_config/dependencies/Testing.kt
+++ version_config/src/main/java/com/flywith24/version_config/dependencies/Testing.kt
@@ -9,5 +9,6 @@ package com.flywith24.version_config.dependencies
 object Testing {
     const val jUnit = "junit:junit:4.12"
     const val androidJunit = "androidx.test.ext:junit:1.1.1"
+    const val androidRunner = "androidx.test:runner:1.2.0"
     const val espresso = "androidx.test.espresso:espresso-core:3.2.0"
 }
