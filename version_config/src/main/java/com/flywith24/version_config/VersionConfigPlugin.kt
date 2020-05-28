ce489b559686e825682f175a6af9e6034d64fe3f
fly_with24 <fly_with24@163.com>
2020-05-28 09:14:16 +0800

    add version config gradle plugin

---
+++ version_config/src/main/java/com/flywith24/version_config/VersionConfigPlugin.kt
@@ -0,0 +1,17 @@
+package com.flywith24.version_config
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author Flywith24
+ * @date   2020/5/28
+ * time   9:02
+ * description
+ * 依赖版本控制插件
+ */
+class VersionConfigPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        //公共配置
+    }
+}
