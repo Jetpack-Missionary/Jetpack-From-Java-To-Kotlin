89670b8b3c53c8599faa3afbb19b9761b1672dc1
KunMinX <kunminx@gmail.com>
2020-06-01 23:39:54 +0800

    add "one more thing" activity

---
+++ jetpack_java/src/main/java/com/kunminx/jetpack_java/sample_one_more_thing/ui/OneMoreThingActivity.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018-2020 KunMinX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kunminx.jetpack_java.sample_one_more_thing.ui;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+
+import com.kunminx.architecture.ui.BaseActivity;
+import com.kunminx.jetpack_java.R;
+import com.kunminx.jetpack_java.databinding.ActivityOneMoreThingBinding;
+import com.kunminx.jetpack_java.sample_one_more_thing.ui.state.OneMoreThingViewModel;
+
+/**
+ * Create by KunMinX at 19/10/16
+ */
+
+public class OneMoreThingActivity extends BaseActivity {
+
+    private OneMoreThingViewModel mOneMoreThingViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mOneMoreThingViewModel = getActivityViewModel(OneMoreThingViewModel.class);
+
+        ActivityOneMoreThingBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_one_more_thing);
+        binding.setLifecycleOwner(this);
+        binding.setVm(mOneMoreThingViewModel);
+    }
+}
---
+++ jetpack_java/src/main/java/com/kunminx/jetpack_java/sample_one_more_thing/ui/state/OneMoreThingViewModel.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018-2020 KunMinX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kunminx.jetpack_java.sample_one_more_thing.ui.state;
+
+import androidx.databinding.ObservableField;
+import androidx.lifecycle.ViewModel;
+
+/**
+ * Create by KunMinX at 2020/6/1
+ */
+public class OneMoreThingViewModel extends ViewModel {
+
+    public final ObservableField<String> pageAssetPath = new ObservableField<>();
+
+    {
+        pageAssetPath.set("summary.html");
+    }
+}
