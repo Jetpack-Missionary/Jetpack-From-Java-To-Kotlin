d82a230746c9b3de3e052b4b0a155894b2fa3960
Flywith24 <fly_with24@163.com>
2020-06-03 21:35:48 +0800

    REQUEST_NEW_MOMENT Constance

--- jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/common_data/Configs.kt
+++ jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/common_data/Configs.kt
@@ -8,6 +8,8 @@ package com.flywith24.jetpack_kotlin.common_data
  */
 object Configs {
     const val REQUEST_LOCATION_INFO = 0x00001
+    const val REQUEST_NEW_MOMENT = 0x00002
+    const val NEW_MOMENT = "NEW_MOMENT"
     const val LOCATION_RESULT = "LOCATION_RESULT"
     const val THIS_MOMENT = "THIS_MOMENT"
 }
