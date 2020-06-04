84ce60ed88b49f5ed0502b0303fc8ed675213f8e
Flywith24 <fly_with24@163.com>
2020-06-04 22:15:13 +0800

    add EventMutableLiveData.kt

---
+++ jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/base/Event.kt
@@ -0,0 +1,32 @@
+package com.flywith24.jetpack_kotlin.base
+
+/**
+ * @author Flywith24
+ * @date   2020/6/4
+ * time   21:10
+ * description
+ * 使用 包装类 解决 LiveData 粘性事件的问题
+ * 详见 https://juejin.im/post/5b2b1b2cf265da5952314b63
+ */
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    fun peekContent(): T = content
+}
---
+++ jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/base/EventMutableLiveData.kt
@@ -0,0 +1,31 @@
+package com.flywith24.jetpack_kotlin.base
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+
+/**
+ * @author Flywith24
+ * @date   2020/6/4
+ * time   21:13
+ * description
+ */
+
+//为 LiveData<Event<T>>提供类型别名，使用 EventLiveData<T> 即可
+typealias EventMutableLiveData<T> = MutableLiveData<Event<T>>
+
+@MainThread
+inline fun <T> EventMutableLiveData<T>.observeEvent(
+        owner: LifecycleOwner,
+        crossinline onChanged: (T) -> Unit
+): Observer<Event<T>> {
+    val wrappedObserver = Observer<Event<T>> { t ->
+        //数据没有被使用过则发送给调用者，否则不处理
+        t.getContentIfNotHandled()?.let { data ->
+            onChanged.invoke(data)
+        }
+    }
+    observe(owner, wrappedObserver)
+    return wrappedObserver
+}
