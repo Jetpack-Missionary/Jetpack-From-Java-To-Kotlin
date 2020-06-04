615c1d72f383cd50fef7a4c1c366601bcfa2993f
Flywith24 <fly_with24@163.com>
2020-06-04 22:16:07 +0800

    use EventMutableLiveData in SharedViewModel.kt

--- jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/sample_05_navigation/ui/callback/SharedViewModel.kt
+++ jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/sample_05_navigation/ui/callback/SharedViewModel.kt
@@ -1,9 +1,8 @@
 package com.flywith24.jetpack_kotlin.sample_05_navigation.ui.callback
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.flywith24.jetpack_kotlin.base.EventMutableLiveData
 import com.flywith24.jetpack_kotlin.common_data.bean.Moment
-import com.kunminx.architecture.bridge.callback.Event
 
 /**
  * @author Flywith24
@@ -16,7 +15,7 @@ class SharedViewModel : ViewModel() {
      * 使用 包装类 解决 LiveData 粘性事件的问题
      * 详见 https://juejin.im/post/5b2b1b2cf265da5952314b63
      */
-    val location = MutableLiveData<Event<String>>()
-    val moment = MutableLiveData<Event<Moment>>()
+    val location = EventMutableLiveData<String>()
+    val moment = EventMutableLiveData<Moment>()
 
 }
