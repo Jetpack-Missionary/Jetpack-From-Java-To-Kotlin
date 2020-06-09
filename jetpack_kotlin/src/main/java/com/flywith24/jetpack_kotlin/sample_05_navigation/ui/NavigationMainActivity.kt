0ead245d3003d4ac4b4958872df7429f082f6f67
Flywith24 <fly_with24@163.com>
2020-06-09 23:59:35 +0800

    remove log

--- jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/sample_05_navigation/ui/NavigationMainActivity.kt
+++ jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/sample_05_navigation/ui/NavigationMainActivity.kt
@@ -1,8 +1,6 @@
 package com.flywith24.jetpack_kotlin.sample_05_navigation.ui
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.flywith24.jetpack_kotlin.R
 import com.flywith24.jetpack_kotlin.databinding.KotlinActivityMainNavigationBinding
@@ -16,11 +14,9 @@ import com.kunminx.architecture.ui.BaseActivity
  * description
  */
 class NavigationMainActivity : BaseActivity() {
-    private val mMainViewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("yyz11", "NavigationMainActivity")
-        val binding = DataBindingUtil.setContentView<KotlinActivityMainNavigationBinding>(this, R.layout.kotlin_activity_main_navigation)
+        DataBindingUtil.setContentView<KotlinActivityMainNavigationBinding>(this, R.layout.kotlin_activity_main_navigation)
     }
 }
