8857f55248a3ce3329e6acc0458b0beec51f798c
Flywith24 <fly_with24@163.com>
2020-06-09 22:50:32 +0800

    add avatar url

--- jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/common_data/APIs.kt
+++ jetpack_kotlin/src/main/java/com/flywith24/jetpack_kotlin/common_data/APIs.kt
@@ -8,5 +8,11 @@ package com.flywith24.jetpack_kotlin.common_data
  */
 object APIs {
     const val PIC_URL = "https://i.loli.net/2020/05/29/Dynh3KrQ7m8Oz4A.jpg"
+    const val KUNMINX_URL = "https://upload.jianshu.io/users/upload_avatars/57036/3e7ae7bc-9b9b-46c8-8082-bb2481d979dc.jpg"
+
+    const val FLYWITH24_URL = "https://upload-images.jianshu.io/upload_images/57036-48e9886afe74ea9b.jpeg"
+
+    const val SCENE_URL = "https://upload-images.jianshu.io/upload_images/57036-99344035bfce65b8.jpeg"
+
     const val ADD_PIC_TIP_URL = "https://upload-images.jianshu.io/upload_images/57036-0c120ced543d506a.png"
 }
