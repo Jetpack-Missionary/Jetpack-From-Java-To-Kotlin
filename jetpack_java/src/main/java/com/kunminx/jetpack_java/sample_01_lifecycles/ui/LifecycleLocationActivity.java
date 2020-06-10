2ad5509659cc00c136c8dc7b55205aa56525bed3
Flywith24 <fly_with24@163.com>
2020-06-10 10:02:56 +0800

    add back logic

--- jetpack_java/src/main/java/com/kunminx/jetpack_java/sample_01_lifecycles/ui/LifecycleLocationActivity.java
+++ jetpack_java/src/main/java/com/kunminx/jetpack_java/sample_01_lifecycles/ui/LifecycleLocationActivity.java
@@ -19,13 +19,14 @@ package com.kunminx.jetpack_java.sample_01_lifecycles.ui;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.kunminx.architecture.ui.BaseActivity;
 import com.kunminx.jetpack_java.R;
 import com.kunminx.jetpack_java.common_data.Configs;
-import com.kunminx.jetpack_java.sample_01_lifecycles.domain.LifecycleLocationManager;
 import com.kunminx.jetpack_java.common_ui.adapter.LocationAdapter;
-import com.kunminx.architecture.ui.BaseActivity;
+import com.kunminx.jetpack_java.sample_01_lifecycles.domain.LifecycleLocationManager;
 
 /**
  * Create by KunMinX at 19/10/16
@@ -34,6 +35,7 @@ import com.kunminx.architecture.ui.BaseActivity;
 public class LifecycleLocationActivity extends BaseActivity {
 
     private RecyclerView mRecyclerView;
+    private Toolbar mToolbar;
     private LocationAdapter mLocationAdapter;
 
     @Override
@@ -41,6 +43,8 @@ public class LifecycleLocationActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_location_lifecycles);
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setNavigationOnClickListener(v -> finish());
 
         mRecyclerView = findViewById(R.id.rv);
 
--- jetpack_java/src/main/java/com/kunminx/jetpack_java/sample_02_livedata/ui/LiveDataLocationActivity.java
+++ jetpack_java/src/main/java/com/kunminx/jetpack_java/sample_02_livedata/ui/LiveDataLocationActivity.java
@@ -19,13 +19,14 @@ package com.kunminx.jetpack_java.sample_02_livedata.ui;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.kunminx.architecture.ui.BaseActivity;
 import com.kunminx.jetpack_java.R;
 import com.kunminx.jetpack_java.common_data.Configs;
 import com.kunminx.jetpack_java.common_ui.adapter.LocationAdapter;
 import com.kunminx.jetpack_java.sample_02_livedata.domain.LiveDataLocationManager;
-import com.kunminx.architecture.ui.BaseActivity;
 
 /**
  * Create by KunMinX at 19/10/16
@@ -34,6 +35,8 @@ import com.kunminx.architecture.ui.BaseActivity;
 public class LiveDataLocationActivity extends BaseActivity {
 
     private RecyclerView mRecyclerView;
+    private Toolbar mToolbar;
+
     private LocationAdapter mLocationAdapter;
 
     @Override
@@ -41,6 +44,8 @@ public class LiveDataLocationActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_location_lifecycles);
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setNavigationOnClickListener(v -> finish());
 
         mRecyclerView = findViewById(R.id.rv);
 
