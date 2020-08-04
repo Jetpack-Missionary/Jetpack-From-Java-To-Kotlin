32c21dc4137ba89850a85a3fea38bfe89652cede
KunMinX <kunminx@gmail.com>
2020-08-04 15:34:35 +0800

    update gradle dependency

--- version_config/src/main/java/com/flywith24/version_config/dependencies/ThirdParty.kt
+++ version_config/src/main/java/com/flywith24/version_config/dependencies/ThirdParty.kt
@@ -20,7 +20,7 @@ object ThirdParty {
     }
 
     object Archi {
-        const val unPeekLiveData = "com.kunminx.archi:unpeeklivedata:3.2.2-beta8"
+        const val unPeekLiveData = "com.kunminx.archi:unpeek-livedata:3.2.3-beta10"
     }
 
     const val roundedImageView = "com.makeramen:roundedimageview:2.3.0"
